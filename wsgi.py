d8e2634e8a632638955c8ae677f3806bbc5fd7c4
Mike Leung <leungwh1@hotmail.com>
2018-03-04 18:45:28 +0800

    Added import access

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
+import access
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return access.decode(access.key, access.coinbase_id)
 
 if __name__ == "__main__":
     application.run()
